10cd004d22bb10e2ba8970c088b6893cccf9a169
ramezHamdiSa'eed <317170274@sha.edu.eg>
2024-03-02 18:06:35 +0200

    set up main activity fragments with bottom navigation bar

--- app/src/main/java/com/example/skycast/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/skycast/views/activities/MainActivity.kt
@@ -2,13 +2,22 @@ package com.example.skycast.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.skycast.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView=findViewById(R.id.bottom_navigation_bar)
+        navController= Navigation.findNavController(this,R.id.fragmentContainerView)
+        NavigationUI.setupWithNavController(bottomNavigationView,navController)
     }
 
     override fun onBackPressed() {
