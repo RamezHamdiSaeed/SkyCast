697636c55bc92bb9397e93f8c3b6a76c96d092b9
ramezHamdiSa'eed <317170274@sha.edu.eg>
2024-03-04 20:32:27 +0200

    setup the rest activities with the main Activity using the drawer

---
+++ app/src/main/java/com/example/skycast/views/activities/AlertsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.skycast.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.skycast.R
+
+class AlertsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alerts)
+    }
+}
--- app/src/main/java/com/example/skycast/views/activities/MainActivity.kt
+++ app/src/main/java/com/example/skycast/views/activities/MainActivity.kt
@@ -1,23 +1,68 @@
 package com.example.skycast.views.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.example.skycast.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var bottomNavigationView: BottomNavigationView
     lateinit var navController: NavController
+    //drawer to be opened and closed
+    lateinit var drawerLayout: DrawerLayout;
+    lateinit var actionBarDrawerToggle:ActionBarDrawerToggle
+    lateinit var navigationView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottomNavigationView=findViewById(R.id.bottom_navigation_bar)
         navController= Navigation.findNavController(this,R.id.fragmentContainerView)
         NavigationUI.setupWithNavController(bottomNavigationView,navController)
+        //drawer
+        drawerLayout=findViewById(R.id.myDrawer)
+        actionBarDrawerToggle= ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView=findViewById(R.id.navigationView)
+        navigationView.setNavigationItemSelectedListener{
+            item ->
+            when (item.itemId){
+            R.id.settingsDrawer->{
+                startActivity(Intent(this,SittingsActivity::class.java))
+            }
+            R.id.alertsDrawer->{
+                startActivity(Intent(this,AlertsActivity::class.java))
+
+            }
+        }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onBackPressed() {
