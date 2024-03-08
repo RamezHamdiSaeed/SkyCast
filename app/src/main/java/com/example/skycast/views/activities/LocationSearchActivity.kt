96f64369a324173ff5045d68b965ce5dd2dfbe29
ramezHamdiSa'eed <317170274@sha.edu.eg>
2024-03-08 19:01:50 +0200

    setup activities for return result or complete procedure later

---
+++ app/src/main/java/com/example/skycast/views/activities/LocationSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.example.skycast.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.skycast.R
+
+class LocationSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_search)
+    }
+}
